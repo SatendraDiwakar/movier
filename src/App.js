ead3990a4a71f7cc0c5fa1d5a788a474b55639ec
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-07-02 16:59:45 +0530

    Structure Files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <Switch>
+        <Route path="/s/:name/" component={Home} />
+        <Route path="/:movie/" component={Home} />
         <Route path="/" component={Home} />
         <Route component={Error} />
       </Switch>
